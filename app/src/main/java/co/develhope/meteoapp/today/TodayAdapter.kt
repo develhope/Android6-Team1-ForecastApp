e3adfd6d7dfb8fa5619e8a3b96e1e873890ecafc
Antonio <catachthonius01@gmail.com>
2023-09-12 17:34:56 +0200

    Filled TodayData data class and started work on the Adapter.

--- app/src/main/java/co/develhope/meteoapp/today/TodayAdapter.kt
+++ app/src/main/java/co/develhope/meteoapp/today/TodayAdapter.kt
@@ -1,7 +1,27 @@
 package co.develhope.meteoapp.today
 
-class TodayAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import co.develhope.meteoapp.databinding.TodayItemBinding
+import android.view.LayoutInflater
+class TodayAdapter(val todayscreen: List<TodayData>) :
+    RecyclerView.Adapter<TodayAdapter.TodayViewHolder>() {
+    class TodayViewHolder(val binding: TodayItemBinding) :
+        RecyclerView.ViewHolder(binding.root)
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodayViewHolder {
+        return TodayViewHolder(
+            TodayItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return todayscreen.size    }
+
+    override fun onBindViewHolder(holder: TodayViewHolder, position: Int) {
+        val model = todayscreen[position]
+        holder.binding.ora.text = model.
+    }
 
 
 }
--- app/src/main/java/co/develhope/meteoapp/today/TodayData.kt
+++ app/src/main/java/co/develhope/meteoapp/today/TodayData.kt
@@ -1,7 +1,28 @@
 package co.develhope.meteoapp.today
 
 data class TodayData(
+    //recyclerview item elements
 
-val
+val ora: String,
+val immaginesole : Int,
+val gradi : String,
+val immaginegoccia : Int,
+val precipitazioni : String,
+val freccia : Int,
+
+    //reciclerview cardview elements
+
+val percepita :String,
+val gradi45 :String,
+val indiceuv : String,
+val indice510 : String,
+val umidita : String,
+val umiditagradi : String,
+val vento : String,
+val ventosse : String,
+val copertura : String,
+val coperturagradi : String,
+val pioggia : String,
+val pioggiacm: String
 
 )
