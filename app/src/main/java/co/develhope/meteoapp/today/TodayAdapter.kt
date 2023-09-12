99fd801704d35c97fb1a63ccf5e1ac8e8f68ac74
Antonio <catachthonius01@gmail.com>
2023-09-12 17:42:12 +0200

    Finished work on TodayAdapter.

--- app/src/main/java/co/develhope/meteoapp/today/TodayAdapter.kt
+++ app/src/main/java/co/develhope/meteoapp/today/TodayAdapter.kt
@@ -4,6 +4,9 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import co.develhope.meteoapp.databinding.TodayItemBinding
 import android.view.LayoutInflater
+import android.view.View
+import co.develhope.meteoapp.R
+
 class TodayAdapter(val todayscreen: List<TodayData>) :
     RecyclerView.Adapter<TodayAdapter.TodayViewHolder>() {
     class TodayViewHolder(val binding: TodayItemBinding) :
@@ -16,12 +19,39 @@ class TodayAdapter(val todayscreen: List<TodayData>) :
     }
 
     override fun getItemCount(): Int {
-        return todayscreen.size    }
+        return todayscreen.size
+    }
 
     override fun onBindViewHolder(holder: TodayViewHolder, position: Int) {
         val model = todayscreen[position]
-        holder.binding.ora.text = model.
+
+        //recyclerveiw item elements
+        holder.binding.ora.text = model.ora
+        holder.binding.immaginesole.setImageResource(R.drawable.sun)
+        holder.binding.gradi.text = model.gradi
+        holder.binding.immaginegoccia.setImageResource(R.drawable.raindrop)
+        holder.binding.precipitazioni.text = model.precipitazioni
+        holder.binding.freccia.setImageResource(R.drawable.arrow)
+
+        //recyclerview cardview elements
+        holder.binding.gradi45.text = model.gradi45
+        holder.binding.indice510.text = model.indice510
+        holder.binding.umiditagradi.text = model.umiditagradi
+        holder.binding.ventosse.text = model.ventosse
+        holder.binding.coperturagradi.text = model.coperturagradi
+        holder.binding.pioggiacm.text = model.pioggiacm
+
+        holder.binding.constraintlayoutItem.setOnClickListener {
+            holder.binding.freccia.animate().apply {
+                duration = 500
+                rotationBy(180f)
+
+                if (holder.binding.cardview.visibility == View.GONE) {
+                    holder.binding.cardview.visibility = View.VISIBLE
+                } else {
+                    holder.binding.cardview.visibility = View.GONE
+                }
+            }
+        }
     }
-
-
 }
