e271efbffba84233309ceb2662b232ba6f57199b
Antonio <catachthonius01@gmail.com>
2023-09-21 19:22:47 +0200

    Created TodayViewHolder.kt and moved ViewHolder elements into it

--- app/src/main/java/co/develhope/meteoapp/today/TodayAdapter.kt
+++ app/src/main/java/co/develhope/meteoapp/today/TodayAdapter.kt
@@ -9,9 +9,7 @@ import android.view.View.VISIBLE
 import co.develhope.meteoapp.R
 
 class TodayAdapter(val todayscreen: List<TodayData>) :
-    RecyclerView.Adapter<TodayAdapter.TodayViewHolder>() {
-    class TodayViewHolder(val binding: TodayItemBinding) :
-        RecyclerView.ViewHolder(binding.root)
+    RecyclerView.Adapter<TodayViewHolder>() {
 
     val openElementIndex = mutableListOf<Int>()
 
@@ -28,35 +26,7 @@ class TodayAdapter(val todayscreen: List<TodayData>) :
     override fun onBindViewHolder(holder: TodayViewHolder, position: Int) {
         val model = todayscreen[position]
 
-        //recyclerveiw item elements
-        holder.binding.todayDateItem.text = model.todayDate
-        holder.binding.todayWeatherIconItem.setImageResource(R.drawable.sun)
-        holder.binding.todayDegreesItem.text = model.todayDegrees
-        holder.binding.todayRainfallPictureItem.setImageResource(R.drawable.raindrop)
-        holder.binding.todayRainfallItem.text = model.todayRainfall
-        holder.binding.todayArrowItem.setImageResource(R.drawable.arrow)
-
-        //recyclerview cardview elements
-        holder.binding.todayPerceivedDegreesItem.text = model.todayPerceivedDegrees
-        holder.binding.todayUvIndexFactorItem.text = model.todayUvIndexFactor
-        holder.binding.todayHumidityFactorItem.text = model.todayHumidityDegrees
-        holder.binding.todayWindFactorItem.text = model.todayWindFactor
-        holder.binding.todayCoverageFactorItem.text = model.todayCoverageFactor
-        holder.binding.todayRainCmItem.text = model.todayRainFactor
-
-        if (position in openElementIndex) {
-            holder.binding.cardview.visibility = VISIBLE
-
-            holder.binding.todayArrowItem.rotation = 180f
-
-        } else {
-            holder.binding.cardview.visibility = GONE
-
-            holder.binding.todayArrowItem.rotation = 0f
-        }
-
-        holder.binding.constraintlayoutItem.setOnClickListener {
-
+        holder.onBind(model, position, openElementIndex) {
             if (position in openElementIndex) {
                 openElementIndex.remove(position)
             } else {
---
+++ app/src/main/java/co/develhope/meteoapp/today/TodayViewHolder.kt
@@ -0,0 +1,42 @@
+package co.develhope.meteoapp.today
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import co.develhope.meteoapp.R
+import co.develhope.meteoapp.databinding.TodayItemBinding
+
+class TodayViewHolder(val binding : TodayItemBinding): ViewHolder(binding.root) {
+
+    fun onBind(model: TodayData, position: Int, openElementIndex: List<Int>, onClick: () -> Unit) {
+        //recyclerveiw item elements
+        binding.todayDateItem.text = model.todayDate
+        binding.todayWeatherIconItem.setImageResource(R.drawable.sun)
+        binding.todayDegreesItem.text = model.todayDegrees
+        binding.todayRainfallPictureItem.setImageResource(R.drawable.raindrop)
+        binding.todayRainfallItem.text = model.todayRainfall
+        binding.todayArrowItem.setImageResource(R.drawable.arrow)
+
+        //recyclerview cardview elements
+        binding.todayPerceivedDegreesItem.text = model.todayPerceivedDegrees
+        binding.todayUvIndexFactorItem.text = model.todayUvIndexFactor
+        binding.todayHumidityFactorItem.text = model.todayHumidityDegrees
+        binding.todayWindFactorItem.text = model.todayWindFactor
+        binding.todayCoverageFactorItem.text = model.todayCoverageFactor
+        binding.todayRainCmItem.text = model.todayRainFactor
+
+        if (position in openElementIndex) {
+            binding.cardview.visibility = View.VISIBLE
+
+            binding.todayArrowItem.rotation = 180f
+
+        } else {
+            binding.cardview.visibility = View.GONE
+
+            binding.todayArrowItem.rotation = 0f
+        }
+
+        binding.constraintlayoutItem.setOnClickListener {
+            onClick()
+        }
+    }
+}
