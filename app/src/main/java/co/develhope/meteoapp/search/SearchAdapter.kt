9966be75d58f4402ac518bdac33475f0da1177da
Giovanni <giovannidevelop@gmail.com>
2023-09-28 16:01:57 +0200

    fix

--- app/src/main/java/co/develhope/meteoapp/search/SearchAdapter.kt
+++ app/src/main/java/co/develhope/meteoapp/search/SearchAdapter.kt
@@ -7,38 +7,73 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import co.develhope.meteoapp.R
 
-class SearchAdapter(private val cityList: List<SearchData>) : RecyclerView.Adapter<SearchAdapter.SearchViewHolder>() {
+class SearchAdapter(
+    private val cityList: MutableList<String>,
+    private val recentSearchList: List<SearchData>,
+    private val onItemClickListener: OnItemClickListener?
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    private var onItemClickListener: OnItemClickListener? = null
-
-    fun setOnItemClickListener(listener: OnItemClickListener) {
-        onItemClickListener = listener
-    }
-
-    inner class SearchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val cityNameTextView: TextView = itemView.findViewById(R.id.search_item_city)
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.search_item, parent, false)
-        return SearchViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
-        val city = cityList[position]
-        holder.cityNameTextView.text = city.name
-
-
-        holder.itemView.setOnClickListener {
-            onItemClickListener?.onItemClick(city)
-        }
-    }
-
-    override fun getItemCount(): Int {
-        return cityList.size
+    companion object {
+        private const val VIEW_TYPE_CITY = 1
+        private const val VIEW_TYPE_RECENT_SEARCH = 2
     }
 
     interface OnItemClickListener {
         fun onItemClick(searchData: SearchData)
+    }
+
+    inner class CityViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val cityNameTextView: TextView = itemView.findViewById(R.id.search_item_city)
+    }
+
+    inner class RecentSearchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position < cityList.size) {
+            VIEW_TYPE_CITY
+        } else {
+            VIEW_TYPE_RECENT_SEARCH
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            VIEW_TYPE_CITY -> {
+                val view =
+                    LayoutInflater.from(parent.context).inflate(R.layout.city_item, parent, false)
+                CityViewHolder(view)
+            }
+
+            VIEW_TYPE_RECENT_SEARCH -> {
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.recent_search_item, parent, false)
+                RecentSearchViewHolder(view)
+            }
+
+            else -> throw IllegalArgumentException("Invalid ViewHolder Type")
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder.itemViewType) {
+            VIEW_TYPE_CITY -> {
+                val cityHolder = holder as CityViewHolder
+                val city = cityList[position]
+                cityHolder.cityNameTextView.text = city
+            }
+
+            VIEW_TYPE_RECENT_SEARCH -> {
+                val recentSearchHolder = holder as RecentSearchViewHolder
+                recentSearchHolder.itemView.setOnClickListener {
+                    val recentSearch = recentSearchList[position - cityList.size]
+                    onItemClickListener?.onItemClick(recentSearch)
+                }
+            }
+        }
+    }
+
+
+    override fun getItemCount(): Int {
+        return cityList.size + recentSearchList.size
     }
 }
--- app/src/main/java/co/develhope/meteoapp/search/SearchFragment.kt
+++ app/src/main/java/co/develhope/meteoapp/search/SearchFragment.kt
@@ -5,46 +5,53 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
-import androidx.navigation.fragment.findNavController
+import android.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import co.develhope.meteoapp.HomeFragment
 import co.develhope.meteoapp.databinding.FragmentSearchBinding
+
 class SearchFragment : Fragment() {
     private var _binding: FragmentSearchBinding? = null
+    private val recentSearchList = mutableListOf<SearchData>()
     private val binding get() = _binding!!
 
     private val cityList = mutableListOf(
-        SearchData("Palermo, Sicilia"),
-        SearchData("Torino, Piemonte"),
-        SearchData("Milano, Lombardia"),
-        SearchData("Napoli, Campania"),
-        SearchData("Genova, Liguria"),
-        SearchData("Catania, Sicilia"),
-        SearchData("Bologna, Emilia-Romagna"),
-        SearchData("Bari, Puglia"),
-        SearchData("Roma, Lazio"),
-        SearchData("Catanzaro, Calabria")
+        "Palermo, Sicilia",
+        "Torino, Piemonte",
+        "Milano, Lombardia",
+        "Napoli, Campania",
+        "Genova, Liguria",
+        "Catania, Sicilia",
+        "Bologna, Emilia-Romagna",
+        "Bari, Puglia",
+        "Roma, Lazio",
     )
 
-
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
 
         val recyclerView: RecyclerView = binding.cityList
-        val adapter = SearchAdapter(cityList)
+        val adapter = SearchAdapter(cityList, recentSearchList, object : SearchAdapter.OnItemClickListener {
+            override fun onItemClick(searchData: SearchData) {
+            }
+        })
+
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = adapter
 
-        adapter.setOnItemClickListener(object : SearchAdapter.OnItemClickListener {
-            override fun onItemClick(searchData: SearchData) {
-                val action = SearchFragmentDirections.actionSearchFragmentToHomeFragment()
-                findNavController().navigate(action)
+        val searchView = binding.searchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                //Da aggiungere filtri se bisogno
+                return false
             }
         })
 
