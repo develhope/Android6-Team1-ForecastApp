cf8a1784dc95fbca5321915930aa3fab7d470d8d
Antonio <catachthonius01@gmail.com>
2023-10-09 17:13:22 +0200

    Continue work on TodayDataRemote; created TodayDataLocal

---
+++ app/src/main/java/co/develhope/meteoapp/data/local/TodayDataLocal.kt
@@ -0,0 +1,20 @@
+package co.develhope.meteoapp.data.local
+
+import org.threeten.bp.OffsetDateTime
+
+class TodayDataLocal : ArrayList<TodayDataLocal.HourlyLocal>() {
+    data class HourlyLocal(
+        val apparentTemperature: Double?,
+        val cloudCover: Int?,
+        val rainChance: Int?,
+        val humidity: Int?,
+        val uvIndex: Double?,
+        val rain: Double?,
+        val temperature2m: Double?,
+        val time: OffsetDateTime,
+        val weathercode: Int?,
+        val windSpeed: Double?,
+        val windDirection: String?,
+        val isDay: Int?
+    )
+}
