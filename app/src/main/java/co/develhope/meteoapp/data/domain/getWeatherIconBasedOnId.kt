3add3b146124ee3947762e2dbd1c7079f80259e0
Antonio <catachthonius01@gmail.com>
2023-10-10 01:08:40 +0200

    Fixed Indentation

--- app/src/main/java/co/develhope/meteoapp/data/domain/getWeatherIconBasedOnId.kt
+++ app/src/main/java/co/develhope/meteoapp/data/domain/getWeatherIconBasedOnId.kt
@@ -1,11 +1,12 @@
 package co.develhope.meteoapp.data.domain
+
 fun getWeatherIconBasedOnId(forecastIndex: Int): WeatherIcon {
     return when (forecastIndex) {
-            0 -> WeatherIcon.SUN
-            1, 2, 3 -> WeatherIcon.SUNCLOUD
-            51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82 -> WeatherIcon.SUNRAINCLOUD
-            4 -> WeatherIcon.RAINDROP
-            5 -> WeatherIcon.ARROW
-            else -> WeatherIcon.UNAVAILABLE
-        }
+        0 -> WeatherIcon.SUN
+        1, 2, 3 -> WeatherIcon.SUNCLOUD
+        51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82 -> WeatherIcon.SUNRAINCLOUD
+        4 -> WeatherIcon.RAINDROP
+        5 -> WeatherIcon.ARROW
+        else -> WeatherIcon.UNAVAILABLE
+    }
 }
